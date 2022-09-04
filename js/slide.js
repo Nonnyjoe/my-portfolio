cfc0128d00ab72f07b57fbfd9fdc32011dbf1c27
Idogwu Emmanuel Chinonso <104998136+Nonnyjoe@users.noreply.github.com>
2022-09-03 18:14:47 -0700

    reponsive layout

--- js/slide.js
+++ js/slide.js
@@ -5,6 +5,7 @@ const contactBtn = document.querySelector(".contact");
 const downBtn = document.querySelector(".move-down");
 const upBtn = document.querySelector(".back-up")
 const aboutContainer = document.querySelector(".about-color1");
+const moreContainer = document.querySelector(".more");
 
 profileBtn.onclick = (() => {
     mobileContainer.style.marginLeft = "0%";
@@ -14,14 +15,14 @@ profileBtn.onclick = (() => {
 });
 
 projectsBtn.onclick = (() => {
-    mobileContainer.style.marginLeft = "-103%";
+    mobileContainer.style.marginLeft = "-105.3%";
     profileBtn.style.color = "#b3b3b3";
     projectsBtn.style.color = "#f37b6b";
     contactBtn.style.color = "#b3b3b3";
 });
 
 contactBtn.onclick = (() => {
-    mobileContainer.style.marginLeft = "-206%";
+    mobileContainer.style.marginLeft = "-209%";
     profileBtn.style.color = "#b3b3b3";
     projectsBtn.style.color = "#b3b3b3";
     contactBtn.style.color = "#f37b6b";
@@ -29,10 +30,13 @@ contactBtn.onclick = (() => {
 
 downBtn.onclick = (() => {
     aboutContainer.style.marginTop = "-31rem";
-    aboutContainer.style.opacity = "0"
+    moreContainer.style.opacity = "1";
+    aboutContainer.style.opacity = "0";
+
 });
 
 upBtn.onclick = (() => {
     aboutContainer.style.opacity = "1"
     aboutContainer.style.marginTop = "";
+    moreContainer.style.opacity = "0";
 });
