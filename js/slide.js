cb7038806f3dbd2d4a962c483ed9d08f51580a08
Idogwu Emmanuel Chinonso <104998136+Nonnyjoe@users.noreply.github.com>
2022-09-12 18:37:26 -0700

    corrections & bug fix

--- js/slide.js
+++ js/slide.js
@@ -11,25 +11,65 @@ const profile2 = document.querySelector(".profile2");
 
 
 profileBtn.onclick = (() => {
+    close();
+    profileColor();
+});
+
+projectsBtn.onclick = (() => {
+    close();
+    projectsColor();
+});
+
+contactBtn.onclick = (() => {
+    close();
+    contactColor();
+});
+
+function profile() {
+    event.preventDefault();
+    close();
+    profileColor();
+}
+
+function projects() {
+    event.preventDefault();
+    close();
+    projectsColor();
+}
+
+function contact() {
+    event.preventDefault();
+    close();
+    contactColor();
+}
+
+function close() {
+    if ($('body').hasClass('offcanvas-menu')) {
+        $('body').removeClass('offcanvas-menu');
+    }
+}
+
+function profileColor() {
     mobileContainer.style.marginLeft = "0%";
     profileBtn.style.color = "#f37b6b";
     projectsBtn.style.color = "#b3b3b3";
     contactBtn.style.color = "#b3b3b3";
-});
+}
 
-projectsBtn.onclick = (() => {
+function projectsColor() {
     mobileContainer.style.marginLeft = "-104vw";
     profileBtn.style.color = "#b3b3b3";
     projectsBtn.style.color = "#f37b6b";
     contactBtn.style.color = "#b3b3b3";
-});
+}
 
-contactBtn.onclick = (() => {
+function contactColor() {
     mobileContainer.style.marginLeft = "-205vw";
     profileBtn.style.color = "#b3b3b3";
     projectsBtn.style.color = "#b3b3b3";
     contactBtn.style.color = "#f37b6b";
-});
+}
+
 
 downBtn.onclick = (() => {
     aboutContainer.style.marginTop = "-31rem";
@@ -44,19 +84,5 @@ upBtn.onclick = (() => {
     moreContainer.style.opacity = "0";
 });
 
-// submit.addEventListener("click", submited);
 
-// function submited() {
-//     setTimeout(function() {
-
-//     }, 1000);
-
-// }
-
-profile2.addEventListener("click", shift);
-
-function shift() {
-    console.log("testing")
-    mobileContainer.style.marginLeft = "3%";
-
-}
+// form submission
